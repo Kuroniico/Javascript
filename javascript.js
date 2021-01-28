db971d5090ffa4c630155dbb76bf3f23656b89d0
FlorianRabaste <70692813+FlorianRabaste@users.noreply.github.com>
2021-01-28 12:48:16 +0100

    J'ai fait les exercices 2,3 et 4

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+//-----Exo1-----\\
+
 function surfaceRectangle(){
     var largeur;
     var longueur;
@@ -7,10 +9,39 @@ function surfaceRectangle(){
     alert("La surface du rectangle est " +longueur*largeur);
     }
 
+//-----Exo2-----\\
+
 function aireCercle(){
-    var rayon;
-    var pi = 3.14;
-    rayon = prompt("Rentrez un rayon");
-    alert("Le périmètre du cercle est"(pi*(rayon*rayon)));
-    alert("L'aire du cercle est");
+    var rayon = parseInt(prompt("Rentrez un rayon"));
+    alert("Le périmètre du cercle est" + ( Math.PI * (rayon*2)));
+    alert("L'aire du cercle est" + ( Math.PI * (rayon*rayon)));
+}
+
+//-----Exo3-----\\
+
+var a = 3;
+var b = 2;
+    
+function multiplie(x=8){
+    return 3*x;
+}
+function affiche(){
+    alert(multiplie(a));
+    alert(multiplie(b));
+}
+
+//-----Exo4-----\\
+var tab = [-2,1,4];
+function additionne(x){
+    return (2+x);
+}
+function affiche2(){
+    alert(additionne(tab[0]));
+    alert(additionne(tab[tab.length-1]));
+}
+
+//-----Exo5-----\\
+
+function boucle(){
+    
 }
