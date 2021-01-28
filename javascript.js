431225ebc2b0d5a052bdaef2bf9e0a510af0424d
FlorianRabaste <70692813+FlorianRabaste@users.noreply.github.com>
2021-01-28 10:44:52 +0100

    first commit

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function surfaceRectangle(){
+    var largeur;
+    var longueur;
+    largeur = prompt("Rentrez la longueur du rectangle");
+    longueur = prompt("Rentrez la largeur du rectangle");
+    alert("Le périmètre vaut "+2*(Number(longueur)+Number(largeur)));
+    alert("La surface du rectangle est " +longueur*largeur);
+    }
+
+function aireCercle(){
+    var rayon;
+    var pi = 3.14;
+    rayon = prompt("Rentrez un rayon");
+    alert("Le périmètre du cercle est"(pi*(rayon*rayon)));
+    alert("L'aire du cercle est");
+}
